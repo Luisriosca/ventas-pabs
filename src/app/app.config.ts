2b690d80f93f665f0a072a5e89b2c13c28f0d466
Diego <119649331+818diego@users.noreply.github.com>
2026-01-12 11:58:39 -0800

    chore(config): se configuro el PrimeNG con el theme Aura para el proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'light',
+          cssLayer: false,
+          ripple: true,
+        }
+      },
+    }),
+    MessageService,
+  ],
 };
